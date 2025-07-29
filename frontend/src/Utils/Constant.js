c1fa3a8a880d8b6845d7c48c7a1dddbb7ecc9c23
Nitesh Giri <niteshgiri@Niteshs-MacBook-Air.local>
2025-07-29 14:10:55 +0530

    updated

--- frontend/src/Utils/Constant.js
+++ frontend/src/Utils/Constant.js
@@ -1,4 +1,4 @@
 
-export const USER_API_END_POINT = "http://localhost:8080/api/v1/user";
-export const AI_API_END_POINT = "http://localhost:8080/api/v1/ai";
-export const WORKSPACE_API_END_POINT = "http://localhost:8080/api/v1/workspace";
+export const USER_API_END_POINT = "https://quick-code-ng.vercel.app/api/v1/user";
+export const AI_API_END_POINT = "https://quick-code-ng.vercel.app/api/v1/ai";
+export const WORKSPACE_API_END_POINT = "https://quick-code-ng.vercel.app/api/v1/workspace";
